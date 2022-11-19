5b5677e166de8dcd38b9f05fce762fd7f7254636
Maria Bykova <mariab@bk.ru>
2022-11-19 15:04:58 +0300

    Добавлены фильтры

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
@@ -13,7 +14,9 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    filter_backends = (filters.SearchFilter,)
     pagination_class = LimitOffsetPagination
+    search_fields = ('author__username',)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -43,6 +46,8 @@ class CommentViewSet(viewsets.ModelViewSet):
 class FollowViewSet(ReadCreateViewSet):
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username', 'user__username')
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
